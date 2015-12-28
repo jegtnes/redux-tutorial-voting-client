6f6a210644c5ddc4e5856a5edef2b91b4335a625
jegtnes <alex.jegtnes@cxpartners.co.uk>
2015-12-28 14:19:16 +0100

    Adding chai to test helpers

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,7 +1,12 @@
 import jsdom from 'jsdom';
 
+import chai from 'chai';
+import chaiImmutable from 'chai-immutable';
+
 const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
 const win = doc.defaultView;
 
 global.document = doc;
 global.window = win;
+
+chai.use(chaiImmutable);
