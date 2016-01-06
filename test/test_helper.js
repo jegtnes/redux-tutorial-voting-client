7f337f4d6668e424f6b01a9e26f94c3757384a60
jegtnes <alex.jegtnes@cxpartners.co.uk>
2016-01-06 21:34:54 +0000

    Fixed issue with jsdom not having access to window variables as globals

--- test/test_helper.js
+++ test/test_helper.js
@@ -9,4 +9,13 @@ const win = doc.defaultView;
 global.document = doc;
 global.window = win;
 
+// Ensure all jsdom window.* properties are available as global variables in Node
+// so they can be used as they would be in a browser
+// (React relies on this to work)
+Object.keys(window).forEach((key) => {
+  if (!(key in global)) {
+    global[key] = window[key];
+  }
+});
+
 chai.use(chaiImmutable);
