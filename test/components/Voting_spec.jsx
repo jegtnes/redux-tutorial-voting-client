d52c3f868e6fcf85d41d3b562243356624781b21
jegtnes <alex.jegtnes@cxpartners.co.uk>
2016-01-16 01:00:02 +0000

    Added failing test for purerendermixin

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -72,4 +72,25 @@ describe('Voting', () => {
     expect(winner).to.be.ok;
     expect(winner.textContent).to.contain('Trainspotting');
   });
+
+  // We're going to be using the PureRenderMixin for a speed boost.
+  // https://facebook.github.io/react/docs/pure-render-mixin.html
+  // Test our component is pure: i.e., given the same props and state,
+  // it will always render the same result
+  it('is a pure component', () => {
+    const votingPair = ['Trainspotting', '28 Days Later'];
+    const component = renderIntoDocument(
+      <Voting pair={votingPair} />
+    );
+
+    let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('Trainspotting');
+
+    votingPair[0] = 'Sunshine';
+
+    component.setProps({votingPair: votingPair});
+
+    firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('Trainspotting');
+  });
 });
