1f75b2aead5e4d649c199c5af57b4da9dda51c69
jegtnes <alex.jegtnes@cxpartners.co.uk>
2016-01-24 01:06:12 +0000

    Added pair of votes to the results screen

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,7 +3,16 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export default React.createClass({
   mixins: ['PureRenderMixin'],
+  getPair: function() {
+    return this.props.pair || [];
+  },
   render: function() {
-    return <h1>Oh hai!</h1>
+    return <div className="results">
+      {this.getPair().map(entry =>
+        <div key={entry} className="entry">
+          <h1>{entry}</h1>
+        </div>
+      )}
+    </div>;
   }
 })
