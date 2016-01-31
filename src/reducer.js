fe37d801133803d0ee8abcc1cadf162366bd662f
jegtnes <alex.jegtnes@cxpartners.co.uk>
2016-01-31 18:48:41 +0000

    Implemented functional reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,17 @@
-export default true;
+import {Map} from 'immutable';
+
+// Merge is apparently a function of Immutable maps
+function setState(state, newState) {
+  return state.merge(newState);
+}
+
+export default function(state = Map(), action) {
+
+  switch(action.type) {
+    case 'SET_STATE':
+    return setState(state, action.state);
+  }
+
+  return state;
+
+}
