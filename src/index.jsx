a75121c097f76d47714597cccd4c3ae40b6e9951
jegtnes <alex.jegtnes@cxpartners.co.uk>
2016-02-01 08:35:16 +0000

    Added a socket connection. Not working yet but hey ho

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import Router, {Route} from 'react-router';
 import {createStore} from 'redux';
 import reducer from './reducer';
 import {Provider} from 'react-redux';
+import io from 'socket.io-client'
 import App from './components/App';
 import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
@@ -17,7 +18,9 @@ store.dispatch({
       tally: {Sunshine: 2}
     }
   }
-})
+});
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
 
 const routes = <Route component={App}>
   <Route path="/results" component={ResultsContainer} />
